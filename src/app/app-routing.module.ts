b6688fe85af6a157642f3dad8c128b70b940b432
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2023-12-16 14:12:55 +0700

    login the first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComponentBranch1Component } from './component-branch1/component-branch1.component';
 import { ComponentBranch2Component } from './component-branch2/component-branch2.component';
-
+import { LogInComponent } from './users/log-in/log-in.component';
 const routes: Routes = [
   {
     path: 'component1', component: ComponentBranch1Component
@@ -10,6 +10,9 @@ const routes: Routes = [
 
   {
     path: 'component2', component: ComponentBranch2Component
+  },
+  {
+    path: 'login', component: LogInComponent
   }
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentBranch1Component } from './component-branch1/component-branch1.component';
 import { ComponentBranch2Component } from './component-branch2/component-branch2.component';
-
+import { LogInComponent } from './users/log-in/log-in.component';
 @NgModule({
   declarations: [
     AppComponent,
     ComponentBranch1Component,
-    ComponentBranch2Component
+    ComponentBranch2Component,
+    LogInComponent,
   ],
   imports: [
     BrowserModule,

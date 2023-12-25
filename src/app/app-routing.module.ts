a621b5157a5c94657066102140a25324df12ae8d
vanhung-xyz <112147951+vanhung-xyz@users.noreply.github.com>
2023-12-25 22:45:23 +0700

    hung update component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ComponentBranch1Component } from './component-branch1/component-branch1.component';
-import { ComponentBranch2Component } from './component-branch2/component-branch2.component';
 import { LogInComponent } from './users/log-in/log-in.component';
 import { SigupComponent } from './users/sigup/sigup.component';
 const routes: Routes = [
-  {
-    path: 'component1', component: ComponentBranch1Component
-  },
 
-  {
-    path: 'component2', component: ComponentBranch2Component
-  },
   {
     path: 'login', component: LogInComponent
   },

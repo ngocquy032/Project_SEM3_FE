838ce3714d867a952819a6a0047429bd8eca0df0
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2023-12-25 21:13:40 +0700

    Quy xoa branch demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentBranch1Component } from './component-branch1/component-branch1.component';
-import { ComponentBranch2Component } from './component-branch2/component-branch2.component';
 import { LogInComponent } from './users/log-in/log-in.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
@@ -12,8 +10,6 @@ import { SigupComponent } from './users/sigup/sigup.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentBranch1Component,
-    ComponentBranch2Component,
     LogInComponent,
     SigupComponent,
   ],

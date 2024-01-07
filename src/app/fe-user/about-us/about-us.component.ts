13a5d56abae35165141d74778e6335a606952f1f
tuankhanh04 <112061637+tuankhanh04@users.noreply.github.com>
2024-01-07 15:12:56 +0700

    khanh create form About us

---
+++ src/app/fe-user/about-us/about-us.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
+})
+export class AboutUsComponent {
+//   contents: { url: string, title: string ,name:string, header:string}[] = [
+//     { 
+//       url: 'assets/images/testimonial/testimonial-1.webp',
+//       name: 'Anais Coulon',
+//       title: 'Actor',
+//       header : "There's nothing would satisfy me much more than a worry-free clean and responsive theme for my high-traffic site."
+//     },
+//     { 
+//       url: 'assets/images/testimonial/testimonial-2.webp',
+//       name: 'Ian Schneider',
+//       title: 'Actor',
+//       header : 'Really good design/documentation, pretty much everything is nicely setup. The best choice for Woocommerce shop.'
+//     },
+//     { 
+//       url: 'assets/images/testimonial/testimonial-3.webp',
+//       name: 'Florence Polla',
+//       title: 'Customer',
+//       header : 'ThemeMove deserves 5 star for theme’s features, design quality, flexibility, customizability and support service!'
+//     },
+//     {
+//       url: 'assets/images/testimonial/testimonial-4.webp',
+//       name: 'Sally Ramsey',
+//       title: 'Reporter',
+//       header : 'Thanks for always keeping your WordPress themes up to date. Your level of support is second to none.'
+
+//     }
+//   ];
+
+//   currentSlide: number =0;
+//   next(){
+//     this.currentSlide= (this.currentSlide +1) % this.contents.length;
+//   };
+//   prev(){
+//     this.currentSlide= (this.currentSlide -1 + this.contents.length) % this.contents.length;
+//   }
+}

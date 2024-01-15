a640f3539570261381ef2230276bfb54a7711b1c
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2024-01-15 21:49:48 +0700

    quy call api product Home

--- src/app/fe-user/home/home.component.ts
+++ src/app/fe-user/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/service/products';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: any[] = [];
+  constructor(private productService: ProductService) { }
+  ngOnInit(): void {
+    this.getProduct();
+  }
 
+  getProduct() {
+    this.productService.getProduct().subscribe(product => {
+
+      this.products = product.slice(0,8);
+      console.log('getProduct: ', this.products);
+    });
+  }
 }
---
+++ src/app/fe-user/home/home.model.ts
@@ -0,0 +1,8 @@
+export interface ProductHome{
+  Title: string;
+  Price: string;
+  Sale: string;
+  path: string;
+
+
+}

8aa49227dac5fd1b0410c3698133dd4c8c401e20
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2024-01-19 08:47:57 +0700

    quy fix add wishift

--- src/app/fe-user/home/home.component.ts
+++ src/app/fe-user/home/home.component.ts
@@ -8,48 +8,46 @@ import { ProductService } from 'src/service/products';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-[x: string]: any;
+  [x: string]: any;
   newProducts: any[] = [];
   productSales: any[] = [];
   productBestSallers: any[] = [];
   productImages: any[] = [];
-  Wishift : any[] = [];
+  Wishift: any[] = [];
   constructor(
     private productService: ProductService,
     private router: Router
-    ) { }
+  ) { }
   ngOnInit(): void {
     this.getProduct();
   }
 
   getProduct() {
     this.productService.getProduct().subscribe(product => {
-      this.newProducts = product.slice(0,8);
-      this.productSales = product.slice(18,26);
-      this.productBestSallers = product.slice(9,17);
+      this.newProducts = product.slice(0, 8);
+      this.productSales = product.slice(18, 26);
+      this.productBestSallers = product.slice(9, 17);
 
     });
   }
 
-  productDetails(productId: string): void{
+  productDetails(productId: string): void {
     this.router.navigate(['/productDetails', productId]);
   }
-  addWishift(product: any) {
-    if (!product || !product.productId) {
-      console.error('Product không hợp lệ!', product);
-      let a = this.Wishift.push({
-        product_id: product.productId,
-        price: product.price,
-        title: product.title,
-        path: product.path
-      });
-      console.log('a',a)
+  addWishift(productId: any) {
+    if (productId ) {
+      console.log('san pham dc them', productId);
+      // this.Wishift.push({
+      //   product_id: product.productId,
+      //   price: product.price,
+      //   title: product.title,
+      //   path: product.path
+      // });
     }
 
     // Thêm sản phẩm vào danh sách mong muốn
-    
+
     // Lưu danh sách mong muốn đã cập nhật trở lại LocalStorage
     localStorage.setItem('wishlist', JSON.stringify(this.Wishift));
-    console.log('Sản phẩm được thêm vào mong muốn:', product.title);
   }
-  }
+}

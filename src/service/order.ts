46420c5365ff27840e6254a99c3334737fe357e2
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2024-01-30 01:26:41 +0700

    quy public api

--- src/service/order.ts
+++ src/service/order.ts
@@ -11,7 +11,7 @@ import { OrderStatusModel } from "src/app/fe-admin/update-order/update-order.mod
 // private UrlVnPay = 'https://arts-be1.azurewebsites.net/api/VnpayPayment';
   private UrlVnPay = 'https://localhost:7055/api/VnpayPayment';
   private UrlAdmin = 'https://arts-be1.azurewebsites.net/api/Orders/orderDTO';
-  private urlOrderByIdUser = 'https://localhost:7055/api/Orders/getOrder'
+  private urlOrderByIdUser = 'https://arts-be1.azurewebsites.net/api/Orders/getOrder'
 
   sendOrder(orderData: any): Observable<any> {
     return this.http.post<any>(this.Url, orderData);
